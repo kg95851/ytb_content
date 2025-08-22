52f208e3564a7adee8e5c99615b8125645a820fe
kg95851 <kg95851@naver.com>
2025-08-23 00:58:32 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     outDir: 'dist', // 빌드 결과물이 저장될 폴더
     rollupOptions: {
       input: {
+        dashboard: resolve(__dirname, 'dashboard.html'),
         main: resolve(__dirname, 'index.html'),
         admin: resolve(__dirname, 'admin.html'),
         details: resolve(__dirname, 'details.html'), // 'details.html' 추가
